8c53c0aa095b396cd15fd601a61b783298b818e1
Vincent Arel-Bundock <varel@umich.edu>
2010-06-14 00:00:00 +0000

    version 0.1.1

--- R/WDI.R
+++ R/WDI.R
@@ -1,13 +1,27 @@
-WDI <- function(country="all", indicator="NY.GNS.ICTR.GN.ZS", start=1960, end=2009, extra=FALSE){
-  DFindicator <- list()
-  DFcountries <- list()
-  for (o in 1:length(indicator)){
-    for (z in 1:length(country)){
-      DFcountries[[z]] <- downloadwdi(country[z], indicator[o], start, end)
+WDI <- function(country="US", indicator="NY.GNS.ICTR.GN.ZS", start=2002, end=2005, extra=FALSE){
+INDICATORS <- list()
+missingind <- NULL
+missing <- 0
+for (z in 1:length(indicator)){
+  COUNTRIES <- list()
+  for (w in 1:length(country)){
+    countrydata <- try(downloadwdi(country[w], indicator[z], start, end))
+    if (class(countrydata)=="data.frame"){
+      COUNTRIES[[w]] <- countrydata
     }
-    DFindicator[[o]] <- do.call("rbind", DFcountries)
+    else{next}
   }
-  ifelse(length(indicator)==1, DF <- DFindicator, DF <- merge_recurse(DFindicator))
-  ifelse(extra==TRUE, DF<-merge(DF, extrainfo()),DF<-DF)
-  return(do.call("rbind", DF))
+  if (length(COUNTRIES)>0){
+    INDICATORS[[(z-missing)]] <- do.call("rbind", COUNTRIES)
+  }
+  else{
+    missing <- missing + 1
+    missingind <- c(missingind, indicator[z])
+    next}
+}
+if(length(missingind)>0){print(paste("Unable to download the following indicator(s):", paste(missingind, collapse=" , ")))}
+if(length(INDICATORS)==1){DF <- INDICATORS[[1]]}
+else{DF <- merge_recurse(INDICATORS)}
+if(extra==TRUE){DF <- merge(DF, extrainfo())}
+return(DF)
 }
--- R/downloadwdi.R
+++ R/downloadwdi.R
@@ -1,31 +1,26 @@
 downloadwdi <- function(country,indicator,start,end){
     url <- paste('http://open.worldbank.org/countries/',country,'/indicators/',indicator,'?date=',start,':',end,'&per_page=2500',sep='')
     
-  ### Number of pages to request (with per_page=2500)
+  ### Find number of pages to request (with per_page=2500)
     ifelse(country=="all", ncalls <- ceiling(230*(end+1-start)/2500), ncalls <- ceiling((length(country)+2)*(end+1-start)/2500))
 
   ### Loop over the necessary number of pages
+  DFLIST <- list()
   for (i in 1:ncalls){
     tempurl <- paste(url,'&page=',i,sep='')
     doc <- xmlTreeParse(tempurl, useInternal = TRUE)
 
     ### Extract
-    DF <- xmlToDataFrame(doc)[,2:4]
-
-    ### iso2 codes
-    iso2c <- unlist(lapply(getNodeSet(doc, "//wb:country"), xmlAttrs))
-
+    DF <- data.frame(xmlToDataFrame(doc)[,2:4],
+                     unlist(lapply(getNodeSet(doc, "//wb:country"), xmlAttrs)))
+    names(DF) <- c("country", indicator, "year", "iso2c")
+    
     ### Clean
-    DF <- data.frame(DF, iso2c)
-    for (y in 1:ncol(DF)){
-      DF[,y] <- as.character(DF[,y])
-    }
-    DF[,indicator] <- as.numeric(DF[,"value"])
-    DF[,"value"] <- NULL
-    DF[,"year"] <- as.numeric(DF[,"date"])
-    DF[,"date"] <- NULL
-    DFLIST <- list()
+    for (y in 1:ncol(DF)){DF[,y] <- as.character(DF[,y])}
+    DF[,indicator] <- as.numeric(DF[,indicator])
+    DF[,"year"] <- as.numeric(DF[,"year"])
     DFLIST[[i]] <- unique(DF)
   }
  return(unique(do.call("rbind", DFLIST)))
 }
+
