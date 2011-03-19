74950379b192e0a9bc9f0653f6f89d898a96f04b
Vincent Arel-Bundock <varel@umich.edu>
2011-03-19 00:00:00 +0000

    version 0.1.4

--- R/WDI.R
+++ R/WDI.R
@@ -1,28 +1,67 @@
-WDI <- function(country="US", indicator="NY.GNS.ICTR.GN.ZS", start=2002, end=2005, extra=FALSE){
-INDICATORS <- list()
-missingind <- NULL
-missing <- 0
-indicator <- sub('^[ \t]+|[ \t]+$', '', indicator)
-for (z in 1:length(indicator)){
-  COUNTRIES <- list()
-  for (w in 1:length(country)){
-    countrydata <- try(downloadwdi(country[w], indicator[z], start, end))
-    if (class(countrydata)=="data.frame"){
-      COUNTRIES[[w]] <- countrydata
+WDI <- function (country = "all", indicator = "NY.GNS.ICTR.GN.ZS", start = 2002, end = 2005, extra = FALSE){
+
+  ## Clean user error
+  indicator <- sub("^[ \t]+|[ fire\t]+$", "", indicator)
+  country <- sub("^[ \t]+|[ \t]+$", "", country)
+
+  ## Download data in XML format for each country and each indicator
+  dat.full <- NULL
+  dat.indicator <- NULL
+  for (a in indicator){
+    for (b in country){
+      daturl <- paste("http://api.worldbank.org/countries/", b, 
+                      "/indicators/", a,
+                      "?date=",start,":",end, 
+                      "&per_page=25000",
+                      "&format=XML",
+                      sep = "")
+      dat <- xmlTreeParse(daturl, useInternal = TRUE)
+      iso2c <- unlist(lapply(getNodeSet(dat, "//wb:country"), xmlAttrs))
+      dat <- xmlToDataFrame(dat)[,2:4]
+      dat$iso2c <- iso2c
+
+      ## Clean to ensure smooth merging
+      colnames(dat) <- c("country","year", a, "iso2c")
+      dat$year <- as.numeric(as.character(dat$year))
+      dat$iso2c <- gsub(" ", "", as.character(dat$iso2c))
+      dat$country <- gsub(" ", "", as.character(dat$country))
+      dat.indicator <- rbind(dat.indicator, dat)
+      daturl <- NULL
     }
-    else{next}
+    
+    ## Merge
+    if(is.null(dat.full)){
+      dat.full <- dat.indicator
+    }else{
+      dat.full <- merge(dat.full, dat.indicator, all=TRUE)
+    }
+    dat.indicator <- NULL
   }
-  if (length(COUNTRIES)>0){
-    INDICATORS[[(z-missing)]] <- do.call("rbind", COUNTRIES)
-  }
-  else{
-    missing <- missing + 1
-    missingind <- c(missingind, indicator[z])
-    next}
+
+  ## Clean data
+  dat.full <- dat.full[order(dat.full$iso2c, dat.full$year), c("country","iso2c", "year", indicator)]
+  for (i in 3:ncol(dat.full)){dat.full[,i] <- as.numeric(as.character(dat.full[,i]))}
+
+  ## Extras
+  if(extra==TRUE){
+	dat.full <- merge(dat.full, extra2010, all.x=TRUE)
 }
-if(length(missingind)>0){print(paste("Unable to download the following indicator(s):", paste(missingind, collapse=" , ")))}
-if(length(INDICATORS)==1){DF <- INDICATORS[[1]]}
-else{DF <- merge_recurse(INDICATORS)}
-if(extra==TRUE){DF <- merge(DF, extrainfo())}
-return(DF)
+  return(dat.full)
+}
+
+WDIsearch <- function(string="gdp", field="N", indicator=NULL){
+  if(is.null(indicator)==FALSE){
+    if(indicator %in% series2010$Series.Code){
+      return(series2010[series2010$Series.Code==indicator, c("Series.Code", "Series.Name", "Short.definition", "Long.definition")])
+      stop()
+    }
+  }
+  if(field=="SD"){
+    dat <- series2010[grep(string, series2010$Short.definition, ignore.case=TRUE), c("Series.Code","Short.definition")]
+  }else if(field=="LD"){
+    dat <- series2010[grep(string, series2010$Long.definition, ignore.case=TRUE), c("Series.Code","Long.definition")]
+  }else{
+    dat <- series2010[grep(string, series2010$Series.Name, ignore.case=TRUE), c("Series.Code","Series.Name")]
+  }
+  return(dat)
 }
